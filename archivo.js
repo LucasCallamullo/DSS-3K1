5a17195132a02e85db1431bb39f9816c7a7b49a5
LucasCallamullo <lucascallamullo@hotmail.com>
2026-04-08 17:25:10 -0300

    change on branch two

--- archivo.js
+++ archivo.js
@@ -1,7 +1,4 @@
 
 
-console.log("Branch  rama_a");
 console.log("Holaaa Mundo");
-console.log("Branch  rama_b");
-console.log("Branch  rama_c");
-console.log("Branch  rama_d");
+console.log("changes on branch 2");
