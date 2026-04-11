0f0ded991a674846b70024eb13aeed9a0dd8adfa
LucasCallamullo <lucascallamullo@hotmail.com>
2026-04-11 17:58:52 -0300

    creada carpeta de branchs practica

--- branchs/archivo.js
+++ branchs/archivo.js
@@ -4,3 +4,8 @@
 console.log("Holaaa Mundo");
 console.log("Branch_1");
 console.log("changes on branch 2");
+setTimeout(() => {
+    console.log("Hola Piizaaa");
+    callback();
+    useCallback();
+});
